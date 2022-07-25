f5a31e96d0f51f5c55c129fb6b06695cdd2e6e40
Goedix <goedix@wearehackerone.com>
2022-07-25 13:10:02 +0200

    Closing GUI when video finish

--- src/main/java/com/github/NGoedix/watchvideo/client/gui/VideoScreen.java
+++ src/main/java/com/github/NGoedix/watchvideo/client/gui/VideoScreen.java
@@ -14,6 +14,11 @@ import nick1st.fancyvideo.api.DynamicResourceLocation;
 import nick1st.fancyvideo.api.MediaPlayerHandler;
 import nick1st.fancyvideo.api.mediaPlayer.MediaPlayerBase;
 import org.jetbrains.annotations.NotNull;
+import uk.co.caprica.vlcj.media.MediaRef;
+import uk.co.caprica.vlcj.media.TrackType;
+import uk.co.caprica.vlcj.player.base.MediaPlayer;
+import uk.co.caprica.vlcj.player.base.MediaPlayerEventListener;
+import uk.co.caprica.vlcj.player.base.TitleDescription;
 
 public class VideoScreen extends AbstractContainerScreen<AbstractContainerMenu> {
 
@@ -25,6 +30,78 @@ public class VideoScreen extends AbstractContainerScreen<AbstractContainerMenu> 
         if (MediaPlayerHandler.getInstance().getMediaPlayer(WatchVideo.getResourceLocation()).providesAPI()) {
             Minecraft.getInstance().getSoundManager().pause();
             MediaPlayerHandler.getInstance().getMediaPlayer(WatchVideo.getResourceLocation()).api().media().prepare(url);
+            MediaPlayerHandler.getInstance().getMediaPlayer(WatchVideo.getResourceLocation()).api().events().addMediaPlayerEventListener(new MediaPlayerEventListener() {
+                @Override
+                public void mediaChanged(MediaPlayer mediaPlayer, MediaRef mediaRef) {}
+                @Override
+                public void opening(MediaPlayer mediaPlayer) {}
+                @Override
+                public void buffering(MediaPlayer mediaPlayer, float v) {}
+                @Override
+                public void playing(MediaPlayer mediaPlayer) {}
+                @Override
+                public void paused(MediaPlayer mediaPlayer) {}
+                @Override
+                public void stopped(MediaPlayer mediaPlayer) {}
+                @Override
+                public void forward(MediaPlayer mediaPlayer) {}
+                @Override
+                public void backward(MediaPlayer mediaPlayer) {}
+                @Override
+                public void stopping(MediaPlayer mediaPlayer) {}
+                @Override
+                public void finished(MediaPlayer mediaPlayer) {
+                    onClose();
+                }
+                @Override
+                public void timeChanged(MediaPlayer mediaPlayer, long l) {}
+                @Override
+                public void positionChanged(MediaPlayer mediaPlayer, float v) {}
+                @Override
+                public void seekableChanged(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void pausableChanged(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void titleListChanged(MediaPlayer mediaPlayer) {}
+                @Override
+                public void titleSelectionChanged(MediaPlayer mediaPlayer, TitleDescription titleDescription, int i) {}
+                @Override
+                public void snapshotTaken(MediaPlayer mediaPlayer, String s) {}
+                @Override
+                public void lengthChanged(MediaPlayer mediaPlayer, long l) {}
+                @Override
+                public void videoOutput(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void elementaryStreamAdded(MediaPlayer mediaPlayer, TrackType trackType, int i, String s) {}
+                @Override
+                public void elementaryStreamDeleted(MediaPlayer mediaPlayer, TrackType trackType, int i, String s) {}
+                @Override
+                public void elementaryStreamUpdated(MediaPlayer mediaPlayer, TrackType trackType, int i, String s) {}
+                @Override
+                public void elementaryStreamSelected(MediaPlayer mediaPlayer, TrackType trackType, String s, String s1) {}
+                @Override
+                public void corked(MediaPlayer mediaPlayer, boolean b) {}
+                @Override
+                public void muted(MediaPlayer mediaPlayer, boolean b) {}
+                @Override
+                public void volumeChanged(MediaPlayer mediaPlayer, float v) {}
+                @Override
+                public void audioDeviceChanged(MediaPlayer mediaPlayer, String s) {}
+                @Override
+                public void chapterChanged(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void programAdded(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void programDeleted(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void programUpdated(MediaPlayer mediaPlayer, int i) {}
+                @Override
+                public void programSelected(MediaPlayer mediaPlayer, int i, int i1) {}
+                @Override
+                public void error(MediaPlayer mediaPlayer) {}
+                @Override
+                public void mediaPlayerReady(MediaPlayer mediaPlayer) {}
+            });
             MediaPlayerHandler.getInstance().getMediaPlayer(WatchVideo.getResourceLocation()).api().audio().setVolume(200);
         }
     }
