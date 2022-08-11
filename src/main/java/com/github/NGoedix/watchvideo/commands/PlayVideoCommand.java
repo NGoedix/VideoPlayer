1a2085de33b4246b62b5ea3186fabebb9fcf3b24
Goedix <goedix@wearehackerone.com>
2022-08-11 13:42:32 +0200

    Fix: Parameter target of PlayVideoCommand added

--- src/main/java/com/github/NGoedix/watchvideo/commands/PlayVideoCommand.java
+++ src/main/java/com/github/NGoedix/watchvideo/commands/PlayVideoCommand.java
@@ -6,18 +6,33 @@ import com.mojang.brigadier.Command;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
+import net.minecraft.commands.arguments.EntityArgument;
+import net.minecraft.network.chat.Component;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.Collection;
 
 public class PlayVideoCommand {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher){
         dispatcher.register(Commands.literal("playvideo")
+                .then(Commands.argument("target", EntityArgument.players())
                 .then(Commands.argument("url", StringArgumentType.greedyString())
-                        .executes(PlayVideoCommand::execute)));
+                .executes(PlayVideoCommand::execute))));
     }
 
     private static int execute(CommandContext<CommandSourceStack> command){
-        PacketHandler.sendToAll(new SendVideoMessage(StringArgumentType.getString(command, "url")));
-        return Command.SINGLE_SUCCESS;
+        Collection<ServerPlayer> players;
+        try {
+            players = EntityArgument.getPlayers(command, "target");
+        } catch (CommandSyntaxException e) {
+            command.getSource().sendFailure(Component.literal("Error with target parameter."));
+            return Command.SINGLE_SUCCESS;
+        }
+        for (ServerPlayer player : players) {
+            PacketHandler.sendTo(new SendVideoMessage(StringArgumentType.getString(command, "url")), player);
+        }return Command.SINGLE_SUCCESS;
     }
 }
