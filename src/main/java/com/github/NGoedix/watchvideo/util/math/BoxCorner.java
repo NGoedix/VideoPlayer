dedbe6529ac6095d3040c01c5e413d6a96524d85
Goedix <goedix+inazucraft@wearehackerone.com>
2023-09-04 13:31:15 +0200

    Fix: Code cleanup

--- src/main/java/com/github/NGoedix/watchvideo/util/math/BoxCorner.java
+++ src/main/java/com/github/NGoedix/watchvideo/util/math/BoxCorner.java
@@ -18,9 +18,8 @@ public enum BoxCorner {
     public BoxCorner neighborOne;
     public BoxCorner neighborTwo;
     public BoxCorner neighborThree;
-    public static final BoxCorner[][] FACING_CORNERS = new BoxCorner[][]{{EDN, EDS, WDN, WDS}, {EUN, EUS, WUN, WUS}, {EUN, EDN, WUN, WDN}, {EUS, EDS, WUS, WDS}, {WUN, WUS, WDN, WDS}, {EUN, EUS, EDN, EDS}};
 
-    private BoxCorner(Facing x, Facing y, Facing z) {
+    BoxCorner(Facing x, Facing y, Facing z) {
         this.x = x;
         this.y = y;
         this.z = z;
@@ -36,50 +35,22 @@ public enum BoxCorner {
         return this.getFacing(facing.axis) == facing;
     }
 
-    public boolean isFacingPositive(Axis axis) {
-        return this.getFacing(axis).positive;
-    }
-
     public Facing getFacing(Axis axis) {
-        switch (axis) {
-            case X:
-                return this.x;
-            case Y:
-                return this.y;
-            case Z:
-                return this.z;
-            default:
-                throw new RuntimeException("null axis not permitted");
-        }
-    }
-
-    public BoxCorner mirror(Axis axis) {
-        switch (axis) {
-            case X:
-                return getCorner(this.x.opposite(), this.y, this.z);
-            case Y:
-                return getCorner(this.x, this.y.opposite(), this.z);
-            case Z:
-                return getCorner(this.x, this.y, this.z.opposite());
-            default:
-                throw new RuntimeException("null axis not permitted");
-        }
+        return switch (axis) {
+            case X -> this.x;
+            case Y -> this.y;
+            case Z -> this.z;
+        };
     }
 
     public Vec3d get(AABB bb) {
         return new Vec3d(this.x.get(bb), this.y.get(bb), this.z.get(bb));
     }
 
-    public static BoxCorner getCornerUnsorted(Facing facing, Facing facing2, Facing facing3) {
-        return getCorner(facing.axis != Axis.X ? (facing2.axis != Axis.X ? facing3 : facing2) : facing, facing.axis != Axis.Y ? (facing2.axis != Axis.Y ? facing3 : facing2) : facing, facing.axis != Axis.Z ? (facing2.axis != Axis.Z ? facing3 : facing2) : facing);
-    }
-
     public static BoxCorner getCorner(Facing x, Facing y, Facing z) {
         BoxCorner[] var3 = values();
-        int var4 = var3.length;
 
-        for(int var5 = 0; var5 < var4; ++var5) {
-            BoxCorner corner = var3[var5];
+        for (BoxCorner corner : var3) {
             if (corner.x == x && corner.y == y && corner.z == z) {
                 return corner;
             }
@@ -88,18 +59,11 @@ public enum BoxCorner {
         return null;
     }
 
-    public static BoxCorner[] faceCorners(Facing facing) {
-        return FACING_CORNERS[facing.ordinal()];
-    }
-
     static {
         BoxCorner[] var0 = values();
-        int var1 = var0.length;
 
-        for(int var2 = 0; var2 < var1; ++var2) {
-            BoxCorner corner = var0[var2];
+        for (BoxCorner corner : var0) {
             corner.init();
         }
-
     }
 }
--- src/main/java/com/github/NGoedix/watchvideo/util/math/VecNf.java
+++ src/main/java/com/github/NGoedix/watchvideo/util/math/VecNf.java
@@ -4,27 +4,15 @@ public abstract class VecNf<T extends VecNf> {
     public VecNf() {
     }
 
-    public VecNf(T vec) {
-        this.set(vec);
-    }
-
     public abstract void set(T var1);
 
     public float get(Axis axis) {
         return this.get(axis.ordinal());
     }
 
-    public void set(Axis axis, float value) {
-        this.set(axis.ordinal(), value);
-    }
-
     public abstract float get(int var1);
 
-    public abstract void set(int var1, float var2);
-
     public abstract int dimensions();
-
-    public abstract T copy();
 
     public abstract void add(T var1);
 
@@ -33,42 +21,9 @@ public abstract class VecNf<T extends VecNf> {
         this.add(vec);
     }
 
-    public abstract void sub(T var1);
-
-    public void sub(T origin, T vec) {
-        this.set(origin);
-        this.sub(vec);
-    }
-
-    public abstract void scale(double var1);
-
-    public void invert() {
-        this.scale(-1.0);
-    }
-
     public abstract boolean equals(Object var1);
 
-    public abstract boolean epsilonEquals(T var1, float var2);
-
-    public boolean epsilonEquals(T vec) {
-        return this.epsilonEquals(vec, 1.0E-4F);
-    }
-
-    public abstract double distance(T var1);
-
-    public abstract double distanceSqr(T var1);
-
     public abstract double length();
-
-    public abstract double lengthSquared();
-
-    public void normalize() {
-        this.scale(1.0 / this.length());
-    }
-
-    public abstract double angle(T var1);
-
-    public abstract float dot(T var1);
 
     public String toString() {
         StringBuilder builder = new StringBuilder();
