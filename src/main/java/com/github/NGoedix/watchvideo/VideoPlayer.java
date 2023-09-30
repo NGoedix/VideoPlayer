db3d0dd30f1525ea25f3c44486718eabdb07acf7
Goedix <goedix+inazucraft@wearehackerone.com>
2023-09-30 14:05:44 +0200

    Feat: 2.6 version

--- src/main/java/com/github/NGoedix/watchvideo/VideoPlayer.java
+++ src/main/java/com/github/NGoedix/watchvideo/VideoPlayer.java
@@ -4,14 +4,20 @@ import com.github.NGoedix.watchvideo.block.ModBlocks;
 import com.github.NGoedix.watchvideo.block.entity.ModBlockEntities;
 import com.github.NGoedix.watchvideo.client.render.TVBlockRenderer;
 import com.github.NGoedix.watchvideo.commands.RegisterCommands;
+import com.github.NGoedix.watchvideo.commands.arguments.SymbolStringArgumentSerializer;
+import com.github.NGoedix.watchvideo.commands.arguments.SymbolStringArgumentType;
 import com.github.NGoedix.watchvideo.common.CommonHandler;
 import com.github.NGoedix.watchvideo.item.ModItems;
 import com.github.NGoedix.watchvideo.util.cache.TextureCache;
 import com.github.NGoedix.watchvideo.util.displayers.VideoDisplayer;
-import com.mojang.logging.LogUtils;
+import me.srrapero720.watermedia.api.image.ImageAPI;
+import me.srrapero720.watermedia.api.image.ImageRenderer;
+import me.srrapero720.watermedia.core.tools.JarTool;
 import net.minecraft.client.renderer.ItemBlockRenderTypes;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.blockentity.BlockEntityRenderers;
+import net.minecraft.commands.synchronization.ArgumentTypeInfos;
+import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
@@ -32,15 +38,17 @@ import org.slf4j.Logger;
 @Mod(Reference.MOD_ID)
 public class VideoPlayer
 {
-    // Directly reference a slf4j logger
-    public static final Logger LOGGER = LogUtils.getLogger();
+
+    private static ImageRenderer IMG_PAUSED;
+
+    public static ImageRenderer pausedImage() { return IMG_PAUSED; }
 
     public static CreativeModeTab videoPlayerTab;
     private static final ResourceLocation VIDEO_PLAYER_TAB_ID = new ResourceLocation(Reference.MOD_ID, "video_player_tab");
 
     public VideoPlayer()
     {
-        LOGGER.info("Initializing mod...");
+        Reference.LOGGER.info("Initializing mod...");
         IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         MinecraftForge.EVENT_BUS.register(RegisterCommands.class);
@@ -61,21 +69,24 @@ public class VideoPlayer
 
     private void onCommonSetup(final FMLCommonSetupEvent event)
     {
+        ArgumentTypeInfos.registerByClass(SymbolStringArgumentType.class, new SymbolStringArgumentSerializer());
         event.enqueueWork(CommonHandler::setup);
     }
 
     private void onClientSetup(FMLClientSetupEvent event) {
         ItemBlockRenderTypes.setRenderLayer(ModBlocks.TV_BLOCK.get(), RenderType.cutout());
         BlockEntityRenderers.register(ModBlockEntities.TV_BLOCK_ENTITY.get(), TVBlockRenderer::new);
+
+        IMG_PAUSED = ImageAPI.renderer(JarTool.readImage(VideoPlayer.class.getClassLoader(), "/pictures/paused.png"), true);
     }
 
     private void onCreativeTabRegistration(CreativeModeTabEvent.Register event) {
-        LOGGER.info("Registering creative tab...");
-        videoPlayerTab = event.registerCreativeModeTab(VIDEO_PLAYER_TAB_ID, (icon) -> new ItemStack(ModBlocks.TV_BLOCK.get()));
+        Reference.LOGGER.info("Registering creative tab...");
+        videoPlayerTab = event.registerCreativeModeTab(VIDEO_PLAYER_TAB_ID, builder -> builder.icon(() -> new ItemStack(ModBlocks.TV_BLOCK.get())).title(Component.translatable("itemGroup.videoplayer.video_player_tab")));
     }
 
     private void addCreative(CreativeModeTabEvent.BuildContents event) {
-        LOGGER.info("Adding items to creative tab...");
+        Reference.LOGGER.info("Adding items to creative tab...");
         event.accept(ModBlocks.TV_BLOCK);
     }
 
