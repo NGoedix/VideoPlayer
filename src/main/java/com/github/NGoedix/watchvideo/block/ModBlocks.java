8ba9cbd102469014d2870bd0b2a8755646f90f68
Goedix <goedix+inazucraft@wearehackerone.com>
2023-09-30 13:18:46 +0200

    Fix: Dynamic light on TV

--- src/main/java/com/github/NGoedix/watchvideo/block/ModBlocks.java
+++ src/main/java/com/github/NGoedix/watchvideo/block/ModBlocks.java
@@ -10,6 +10,8 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.material.Material;
 import net.minecraft.world.level.material.MaterialColor;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,13 +20,18 @@ import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 
 import java.util.function.Supplier;
+import java.util.function.ToIntFunction;
 
 public class ModBlocks {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Reference.MOD_ID);
 
     public static final RegistryObject<Block> TV_BLOCK = registerBlock("tv_block",
-            () -> new TVBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).noOcclusion().requiresCorrectToolForDrops().sound(SoundType.METAL).strength(3.5F, 6.0F)), VideoPlayerModTab.ALL);
+            () -> new TVBlock(BlockBehaviour.Properties.of(Material.HEAVY_METAL).noOcclusion().requiresCorrectToolForDrops().sound(SoundType.METAL).lightLevel(litBlockEmission(12)).strength(3.5F, 6.0F)), VideoPlayerModTab.ALL);
+
+    private static ToIntFunction<BlockState> litBlockEmission(int pLightValue) {
+        return (blockstate) -> blockstate.getValue(BlockStateProperties.LIT) ? pLightValue : 0;
+    }
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block,
                                                                             CreativeModeTab tab) {
