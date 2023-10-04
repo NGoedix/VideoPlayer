33b98794600a77f100dc2f092b5f5f241a8a2dce
Goedix <goedix+inazucraft@wearehackerone.com>
2023-10-04 16:49:40 +0200

    Feat: Updated muted processor

--- src/main/java/com/github/NGoedix/watchvideo/client/gui/VideoScreen.java
+++ src/main/java/com/github/NGoedix/watchvideo/client/gui/VideoScreen.java
@@ -44,7 +44,6 @@ public class VideoScreen extends AbstractContainerScreen<AbstractContainerMenu> 
     boolean closing = false;
     boolean paused = false;
     float volume;
-    boolean muted = false;
     boolean controlBlocked;
 
     // TOOLS
@@ -242,12 +241,10 @@ public class VideoScreen extends AbstractContainerScreen<AbstractContainerMenu> 
 
         // M to mute
         if (pKeyCode == 77) {
-            if (!muted) {
+            if (!player.raw().mediaPlayer().audio().isMute()) {
                 player.mute();
-                muted = true;
             } else {
                 player.unmute();
-                muted = false;
             }
         }
 
